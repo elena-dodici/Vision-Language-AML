c49a8d62ebd486d29cc368906cac8d11a6ac109d
elena-dodici <s229114@studenti.polito.it>
2023-01-28 17:54:11 +0100

    LFbug

--- experiments/domain_disentangle.py
+++ experiments/domain_disentangle.py
@@ -1,12 +1,11 @@
-
-#TODO
+# TODO
 import torch
 from models.base_model import DomainDisentangleModel
 from my_loss import DomainDisentangleLoss
 
 
-class DomainDisentangleExperiment: # See point 2. of the project
-    
+class DomainDisentangleExperiment:  # See point 2. of the project
+
     def __init__(self, opt):
         self.opt = opt
         self.device = torch.device('cpu' if opt['cpu'] else 'cuda:0')
@@ -23,17 +22,18 @@ class DomainDisentangleExperiment: # See point 2. of the project
         self.nll_loss = torch.nn.NLLLoss()
         self.cross_entropy = torch.nn.CrossEntropyLoss()
         self.rec_loss = torch.nn.MSELoss()
-        self.alpha1 = torch.nn.Parameter(torch.tensor(0.1,device='cuda'), requires_grad=True)
-        self.alpha2 = torch.nn.Parameter(torch.tensor(0.1,device='cuda'), requires_grad=True)
-        self.w1 = 1 #主要训练category分类器 所以他的权重高一点，其他权重低一点
+        self.alpha1 = torch.nn.Parameter(torch.tensor(0.1, device='cuda'), requires_grad=True)
+        self.alpha2 = torch.nn.Parameter(torch.tensor(0.1, device='cuda'), requires_grad=True)
+        self.w1 = 1  # 主要训练category分类器 所以他的权重高一点，其他权重低一点
         self.w2 = 1
         self.w3 = 1
         # Setup optimization procedure
-        self.optimizer = torch.optim.Adam(list(self.model.parameters())+[self.alpha1,self.alpha2], lr=opt['lr'])
+        self.optimizer = torch.optim.Adam(list(self.model.parameters()) + [self.alpha1, self.alpha2], lr=opt['lr'])
         # self.optimizer = torch.optim.Adam(self.model.parameters(), lr=opt['lr'])
         # self.optimizer2 = torch.optim.Adam(self.criterion.parameters(), lr=opt['lr'])
         # print("model parameters: ",self.model.parameters())
         # print("criterion parameters: ",self.criterion.parameters())
+
     def entropy_loss(self, f):  # 应该返回一个标量 最后是求和的
 
         # f = torch.clamp_min_(f,0.0001)
@@ -41,6 +41,7 @@ class DomainDisentangleExperiment: # See point 2. of the project
         logf = torch.log(f)
         mlogf = logf.mean(dim=0)
         return -mlogf.sum()
+
     def save_checkpoint(self, path, epoch, iteration, best_accuracy, total_train_loss):
         checkpoint = {}
 
@@ -66,6 +67,7 @@ class DomainDisentangleExperiment: # See point 2. of the project
         self.optimizer.load_state_dict(checkpoint['optimizer'])
 
         return epoch, iteration, best_accuracy, total_train_loss
+
     def train_iteration(self, data_source, data_target):
         # [x]: source/target的图
         # [y]:  category label(狗，猫...)
@@ -79,47 +81,79 @@ class DomainDisentangleExperiment: # See point 2. of the project
         y_s = y_s.to(self.device)
         yd_s = yd_s.to(self.device)
 
-        x_t = x_t.to(self.device) # [32,3,224,224] 32是一个batch中图片数量
+        x_t = x_t.to(self.device)  # [32,3,224,224] 32是一个batch中图片数量
         yd_t = yd_t.to(self.device)
 
         # x = torch.cat((x_s,x_t),0) # [64,3,224,224]
         # yd = torch.cat((yd_s,yd_t),0) # [64] 前32个是source domain label， 后32个是target
 
+        # train source domain 部分
         fG, fG_hat, Cfcs, DCfcs, DCfds, Cfds = self.model(x_s)
         # loss = self.criterion(fG, fG_hat, Cfcs, DCfcs, DCfds, Cfds, y_s, yd)  # 这里要重新写！！！ 不能 直接模型处理完结果传到损失函数里，损失函数可能用的总体模型中不同阶段的输出，而不是最终整体的输出！！！！
-        l_class = self.cross_entropy(Cfcs,y_s)
+
+        # 1: object clr
+        l_class = self.cross_entropy(Cfcs, y_s)
+        l_class.backward(retain_graph=True)
+
+        # 2: domain clr with feature from obj encoder
+        self.freezeLayer(self.model.domain_classifier, True)
+        # no calculation of loss
         l_class_ent_1 = self.entropy_loss(DCfcs)
-
+        l_class_ent_1.backward(retain_graph=True)
+        self.updateOpt()
+        self.freezeLayer(self.model.domain_classifier, False)
+        # update parameters
+        self.optimizer.step()
+        # 3: domain clr
+        # with torch.autograd.set_detect_anomaly(True):
         l_domain_1 = self.cross_entropy(DCfds, yd_s)
+        l_domain_1.backward(retain_graph=True)
+
+        # 4: obj clr with feature from dom encoder
+        self.freezeLayer(self.model.category_classifier, True)
         l_domain_ent_1 = self.entropy_loss(Cfds)
+        # l_domain_ent_1.backward(retain_graph=True)
+        self.updateOpt()
+        self.freezeLayer(self.model.category_classifier, False)
+        self.optimizer.step()
 
-        l_rec_1 = self.rec_loss(fG,fG_hat)
-
-
+        l_rec_1 = self.rec_loss(fG, fG_hat)
+        # l_rec_1.backwards()
+        # train target domain 部分
         fG, fG_hat, _, _, DCfds, Cfds = self.model(x_t)
 
-        l_class_ent_2 = self.entropy_loss(DCfcs) #注释掉这个
-        L_class = l_class + self.alpha1* (l_class_ent_1 + l_class_ent_2)
+        # l_class_ent_2 = self.entropy_loss(DCfcs) #注释掉这个
 
-        l_domain_2 = self.cross_entropy(DCfds,yd_t)
+        L_class = l_class + self.alpha1 * (l_class_ent_1)
+
+        l_domain_2 = self.cross_entropy(DCfds, yd_t)
         l_domain = l_domain_1 + l_domain_2
 
         l_domain_ent_2 = self.entropy_loss(Cfds)
-        L_domain = l_domain + self.alpha2*(l_domain_ent_1 + l_domain_ent_2)
+        L_domain = l_domain + self.alpha2 * (l_domain_ent_1 + l_domain_ent_2)
 
-        l_rec_2 = self.rec_loss(fG,fG_hat)
+        l_rec_2 = self.rec_loss(fG, fG_hat)
         L_rec = l_rec_1 + l_rec_2
 
-        loss =self.w1 * L_class + self.w2 * L_domain + self.w3 * L_rec
-        # if loss < 0 :
-        # print(L_class,'-',l_class_ent_1)
-        # print(L_domain,'-',l_domain_ent_1,'-',l_domain_ent_2)
-            # print(L_rec)
-        # print(loss,L_domain)
-        self.optimizer.zero_grad()
-        loss.backward()
+        self.freezeLayer(self.model.category_classifier, True)
+        self.freezeLayer(self.model.domain_classifier, True)
+        loss = self.w1 * L_class + self.w2 * L_domain + self.w3 * L_rec
+
+        # loss.backward()
+        # freeze  2 clr again
+        self.updateOpt()
         self.optimizer.step()
+        self.freezeLayer(self.model.category_classifier, False)
+        self.freezeLayer(self.model.domain_classifier, False)
         return loss.item()
+
+    def freezeLayer(self, layerName, setState):
+        for param in layerName.parameters():
+            param.requires_grad = not setState
+
+    def updateOpt(self):
+        self.optimizer.param_groups[0]['params'] = [p for p in self.model.parameters() if p.requires_grad]
+        self.optimizer.zero_grad()
 
     def validate(self, loader):
         self.model.eval()  # 设置为evaluation 模式
--- models/base_model.py
+++ models/base_model.py
@@ -2,28 +2,30 @@ import torch
 import torch.nn as nn
 from torchvision.models import resnet18
 
+
 class FeatureExtractor(nn.Module):
     def __init__(self):
         super(FeatureExtractor, self).__init__()
         self.resnet18 = resnet18(pretrained=True)
-    
+
     def forward(self, x):
         # x shape: Batch_size * Channel * H * W #  32*3*224*224
-        x = self.resnet18.conv1(x) # 32*64*112*112
-        x = self.resnet18.bn1(x) # 32*64*112*112
+        x = self.resnet18.conv1(x)  # 32*64*112*112
+        x = self.resnet18.bn1(x)  # 32*64*112*112
         x = self.resnet18.relu(x)
-        x = self.resnet18.maxpool(x) # 32*64*56*56
-        x = self.resnet18.layer1(x) # 32*64*56*56
-        x = self.resnet18.layer2(x) # 32*128*28*28
-        x = self.resnet18.layer3(x) # 32*256*14*14
-        x = self.resnet18.layer4(x) # 32*512*7*7
-        x = self.resnet18.avgpool(x) # 32*512*1*1
+        x = self.resnet18.maxpool(x)  # 32*64*56*56
+        x = self.resnet18.layer1(x)  # 32*64*56*56
+        x = self.resnet18.layer2(x)  # 32*128*28*28
+        x = self.resnet18.layer3(x)  # 32*256*14*14
+        x = self.resnet18.layer4(x)  # 32*512*7*7
+        x = self.resnet18.avgpool(x)  # 32*512*1*1
         x = x.squeeze()
 
         if len(x.size()) < 2:
             x = x.unsqueeze(0)
 
         return x
+
 
 class BaselineModel(nn.Module):
     def __init__(self):
@@ -40,16 +42,17 @@ class BaselineModel(nn.Module):
             nn.ReLU(),
 
             nn.Linear(512, 512),
-            nn.BatchNorm1d(512), # encoder就是得到一个vector，classifer就是把这个vector经过全连接得到n个类
+            nn.BatchNorm1d(512),  # encoder就是得到一个vector，classifer就是把这个vector经过全连接得到n个类
             nn.ReLU()
         )
-        self.classifier = nn.Linear(512, 7) # 全连接
+        self.classifier = nn.Linear(512, 7)  # 全连接
 
     def forward(self, x):
         x = self.feature_extractor(x)
         x = self.category_encoder(x)
         x = self.classifier(x)
         return x
+
 
 class DomainDisentangleModel(nn.Module):
     def __init__(self):
@@ -88,16 +91,16 @@ class DomainDisentangleModel(nn.Module):
         )
 
         self.domain_classifier = nn.Sequential(
-            nn.Linear(512,64),
+            nn.Linear(512, 64),
             nn.LeakyReLU(),
 
-            nn.Linear(64,4),
+            nn.Linear(64, 4),
             # nn.LeakyReLU() # 会出现负数，后面求log会有nan
             # nn.ReLU()
             # nn.Softmax(dim=1)
         )
         self.category_classifier = nn.Sequential(
-            nn.Linear(512,7),
+            nn.Linear(512, 7),
             # nn.ReLU()
             # nn.BatchNorm1d(7),
             # nn.LogSoftmax(dim=1)
@@ -105,27 +108,27 @@ class DomainDisentangleModel(nn.Module):
         )
 
         self.reconstructor = nn.Sequential(
-            nn.Linear(1024,512),
+            nn.Linear(1024, 512),
             nn.ReLU()
         )
 
-    def forward(self, x): # xd包含source+target domain的图
+    def forward(self, x):  # xd包含source+target domain的图
 
-        x = self.feature_extractor(x) # 没有 category label的也正常参加处理，只是计算loss，更新梯度时排除掉
+        x = self.feature_extractor(x)  # 没有 category label的也正常参加处理，只是计算loss，更新梯度时排除掉
         fcs = self.category_encoder(x)
         fds = self.domain_encoder(x)
         # need to return
-        fG_hat = torch.cat((fds,fcs),dim=1)
+        fG_hat = torch.cat((fds, fcs), dim=1)
         fG_hat = self.reconstructor(fG_hat)
         # print(fG_hat.shape)
-        Cfcs = self.category_classifier(fcs) # 经过classifier之后再传出去，nn自带的CrossEntropy本身包括了logSoftmax的计算
-        DCfcs = self.domain_classifier(fcs) #??????????? 这个要放外面算？？？，要冻住DC，反向传播不能更新domain_classifier
+        Cfcs = self.category_classifier(fcs)  # 经过classifier之后再传出去，nn自带的CrossEntropy本身包括了logSoftmax的计算
+        DCfcs = self.domain_classifier(fcs)  # ??????????? 这个要放外面算？？？，要冻住DC，反向传播不能更新domain_classifier
 
         DCfds = self.domain_classifier(fds)
         Cfds = self.category_classifier(fds)
 
         eps = torch.tensor(1e-5, dtype=torch.float)
-        Cfds = torch.clamp(Cfds,min=eps,max=1)
-        DCfcs = torch.clamp(DCfcs,min=eps,max=1)
+        Cfds = torch.clamp(Cfds, min=eps, max=1)
+        DCfcs = torch.clamp(DCfcs, min=eps, max=1)
         return x, fG_hat, Cfcs, DCfcs, DCfds, Cfds
 
